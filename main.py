7a954b085fb534f874eeb63af0e2277a4622acb8
bradyDarren <dcgbfinancial@gmail.com>
2025-03-28 22:02:45 -0500

    creation of the for loop

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ API_KEY = '16fb8560f0c501abec59b9b2768be345'
 parameters = {
     'lat': 29.7589,
     'lon': -95.3677,
-    'appid': API_KEY
+    'appid': API_KEY,
+    'cnt':4
 }
 
 response = requests.get(url='https://api.openweathermap.org/data/2.5/forecast??', params=parameters)
 response.raise_for_status()
 
 data = response.json()
+weather_data = data['list']
+
+unbrella_needed = False
+
+for time in range(len(weather_data)):
+    weather_id = weather_data[time]['weather'][0]['id']
+    if weather_id < 700 and not unbrella_needed: 
+        print('Bring your umbrella')
+        unbrella_needed = True
+    
